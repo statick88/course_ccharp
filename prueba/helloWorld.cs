da3b658c95ef59a7c79619c7047ec544b1bebdff
statick88 <dsaavedra88@gmail.com>
2023-11-17 16:45:53 +0000

    initial commit

---
+++ prueba/helloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo desde la ESPE!");
+        }
+    }
+}
